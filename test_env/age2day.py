2d884e38322351d8e373064349c546ec2809766e
Sumit <mail.sumit.choudhary@gmail.com>
2025-01-17 10:46:54 -0500

    manually commited

---
+++ test_env/age2day.py
@@ -0,0 +1,9 @@
+def age2day(value):
+    value=value * 365
+    return value
+
+
+if __name__ == "__main__":
+    age = 65
+    #age2day(age)
+    print(age2day(age))
---
+++ test_env/numberLength.py
@@ -0,0 +1,8 @@
+# calculate length of a number
+
+def calculateLength(number):
+    return len(str(number))
+
+if __name__ == "__main__":
+    number = 123456789
+    print(calculateLength(number))
